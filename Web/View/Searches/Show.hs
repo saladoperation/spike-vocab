9e79d8001c5adbe6235e5ffebdc46de6901c6783
Your Name <you@example.com>
2022-04-26 10:11:17 +0000

    Render keyword

--- Web/View/Searches/Show.hs
+++ Web/View/Searches/Show.hs
@@ -1,13 +1,22 @@
 module Web.View.Searches.Show where
 import Web.View.Prelude
 
-data ShowView = ShowView { keyword :: Text, entries :: [Entry] }
+data ShowView = ShowView { keyword :: Text, match :: Bool, entries :: [Entry] }
 
 instance View ShowView where
     html ShowView { .. } = [hsx|
-        <ol>{forEach entries renderEntry}</ol>
+        <ol>
+            {renderKeyword keyword match}
+            {forEach entries renderEntry}
+        </ol>
     |]
         where
             renderEntry entry = [hsx|
                 <li>{get #title entry}</li>
             |]
+
+renderKeyword keyword match = if match
+    then [hsx||]
+    else [hsx|
+    <li>{keyword}</li>
+    |]
