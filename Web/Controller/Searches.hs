44313d218335b24ae0dae161063396a0b55cb993
Your Name <you@example.com>
2022-04-26 22:26:48 +0000

    Extract maxOutputLength

--- Web/Controller/Searches.hs
+++ Web/Controller/Searches.hs
@@ -2,10 +2,14 @@ module Web.Controller.Searches where
 
 import Web.Controller.Prelude
 import Web.View.Searches.Show
+import Database.PostgreSQL.Simple.Types
+
+maxOutputLength :: Int
+maxOutputLength = 4
 
 instance Controller SearchesController where
     action ShowSearchAction { keyword } = do
         entry <- query @Entry |> findMaybeBy #title keyword
         let match = isJust entry
-        entries :: [Entry] <- sqlQuery "SELECT * FROM entries ORDER BY SIMILARITY(METAPHONE(title, 4), METAPHONE(?, 4)) DESC" (Only keyword)
+        entries :: [Entry] <- sqlQuery "SELECT * FROM entries ORDER BY SIMILARITY(METAPHONE(title, ?), METAPHONE(?, ?)) DESC" (maxOutputLength, keyword, maxOutputLength)
         render ShowView { .. }
