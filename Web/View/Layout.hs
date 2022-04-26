c03a1680cac9ff7c7bc81da2431069fb9c0b9a4e
Your Name <you@example.com>
2022-04-26 03:55:30 +0000

    Add a navbar

--- Web/View/Layout.hs
+++ Web/View/Layout.hs
@@ -23,6 +23,7 @@ defaultLayout inner = H.docTypeHtml ! A.lang "en" $ [hsx|
 <body>
     <div class="container mt-4">
         {renderFlashMessages}
+        {navbar}
         {inner}
     </div>
 </body>
@@ -71,3 +72,27 @@ metaTags = [hsx|
     <meta property="og:description" content="TODO"/>
     {autoRefreshMeta}
 |]
+
+navbar :: Html
+navbar = [hsx|
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">IHP Blog</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+      </li>
+    </ul>
+    {loginLogoutButton}
+  </div>
+</nav>
+|]
+    where
+        loginLogoutButton :: Html
+        loginLogoutButton =
+            case fromFrozenContext @(Maybe User) of
+                Just user -> [hsx|<a class="js-delete js-delete-no-confirm text-secondary" href={DeleteSessionAction}>Logout</a>|]
+                Nothing -> [hsx|<a class="text-secondary" href={NewSessionAction}>Login</a>|]
