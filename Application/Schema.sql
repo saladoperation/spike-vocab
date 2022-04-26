fc06d30412cf8c1a4bd0726e1c5369063554f9ab
Your Name <you@example.com>
2022-04-26 22:45:42 +0000

    Add examples

--- Application/Schema.sql
+++ Application/Schema.sql
@@ -10,3 +10,14 @@ CREATE TABLE entries (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
     title TEXT NOT NULL UNIQUE
 );
+CREATE TABLE examples (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+    user_id UUID NOT NULL,
+    entry_id UUID NOT NULL,
+    youtube_id TEXT NOT NULL,
+    "start" INT NOT NULL
+);
+CREATE INDEX examples_user_id_index ON examples (user_id);
+CREATE INDEX examples_entry_id_index ON examples (entry_id);
+ALTER TABLE examples ADD CONSTRAINT examples_ref_entry_id FOREIGN KEY (entry_id) REFERENCES entries (id) ON DELETE NO ACTION;
+ALTER TABLE examples ADD CONSTRAINT examples_ref_user_id FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE NO ACTION;
