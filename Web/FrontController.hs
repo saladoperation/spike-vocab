5ef38b0a1b36e6ca8e422fb51d0c04a779786c1f
Your Name <you@example.com>
2022-04-26 03:49:58 +0000

    Mount session controller

--- Web/FrontController.hs
+++ Web/FrontController.hs
@@ -3,6 +3,8 @@ module Web.FrontController where
 import IHP.RouterPrelude
 import Web.Controller.Prelude
 import Web.View.Layout (defaultLayout)
+import IHP.LoginSupport.Middleware
+import Web.Controller.Sessions
 
 -- Controller Imports
 import Web.Controller.Static
@@ -10,6 +12,7 @@ import Web.Controller.Static
 instance FrontController WebApplication where
     controllers = 
         [ startPage WelcomeAction
+        , parseRoute @SessionsController
         -- Generator Marker
         ]
 
@@ -17,3 +20,4 @@ instance InitControllerContext WebApplication where
     initContext = do
         setLayout defaultLayout
         initAutoRefresh
+        initAuthentication @User
