d81dbd19d89b7425ac75757116a4cb3cbf6fe826
Your Name <you@example.com>
2022-04-26 09:29:31 +0000

    Add action to navbar's form

--- Web/View/Layout.hs
+++ Web/View/Layout.hs
@@ -80,8 +80,8 @@ navbar = [hsx|
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <form class="form-inline my-2 my-lg-0">
-    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+  <form method="GET" action={ShowSearchAction ""} class="form-inline my-2 my-lg-0" >
+    <input name="keyword" class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
   </form>
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
