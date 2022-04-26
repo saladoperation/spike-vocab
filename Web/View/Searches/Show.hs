174509cea89d43c6bae2ba52d71ba99609cde283
Your Name <you@example.com>
2022-04-26 23:03:16 +0000

    Add a link to search results

--- Web/View/Searches/Show.hs
+++ Web/View/Searches/Show.hs
@@ -18,5 +18,5 @@ instance View ShowView where
 renderKeyword keyword match = if match
     then [hsx||]
     else [hsx|
-    <li>{keyword}</li>
+    <li><a href={NewEntryAction}>{keyword}</a></li>
     |]
