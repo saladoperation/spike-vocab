1efa75f98f0a4577ef90b520858cf6e1da2b3328
Your Name <you@example.com>
2022-04-26 03:40:41 +0000

    Define CurrentUserRecord

--- Web/Types.hs
+++ Web/Types.hs
@@ -3,8 +3,14 @@ module Web.Types where
 import IHP.Prelude
 import IHP.ModelSupport
 import Generated.Types
+import IHP.LoginSupport.Types
 
 data WebApplication = WebApplication deriving (Eq, Show)
 
 
 data StaticController = WelcomeAction deriving (Eq, Show, Data)
+
+instance HasNewSessionUrl User where
+    newSessionUrl _ = "/NewSession"
+
+type instance CurrentUserRecord = User
