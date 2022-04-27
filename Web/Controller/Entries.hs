137f5d8349d3b2251211480679fbc7826d0d2b66
Your Name <you@example.com>
2022-04-27 04:25:17 +0000

    Define maybeExample

--- Web/Controller/Entries.hs
+++ Web/Controller/Entries.hs
@@ -3,10 +3,31 @@ module Web.Controller.Entries where
 import Web.Controller.Prelude
 import Web.View.Entries.Show
 import Web.View.Entries.New
+import qualified Data.Text as T
+import qualified Network.URI as URI
+import qualified Data.Text.Read as Read
 
 instance Controller EntriesController where
     action CreateEntryAction = do
         let entry = newRecord @Entry
+        let maybeExample = do
+            uri <- param @Text "url"
+                |> T.unpack
+                |> URI.parseURI
+            let youtubeId = URI.uriPath uri
+                            |> T.pack
+                            |> T.tail
+            start <- case URI.uriQuery uri
+                        |> T.pack
+                        |> T.drop 3
+                        |> Read.decimal of
+                        Right (start, _) -> Just start
+                        Left _ -> Nothing
+            newRecord @Example
+                |> set #userId currentUserId
+                |> set #youtubeId youtubeId
+                |> set #start start
+                |> pure
         entry
             |> fill @'["title"]
             |> ifValid \case
